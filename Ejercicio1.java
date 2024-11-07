6ea8b8e30d33033745155bc3020e2ea7b97bd60c
OsmerDavidCardonaMejia <juliomidencearias2000@gmail.com>
2024-11-06 19:25:08 -0600

    Hola
    Crear programa donde se crea el nombre.

---
+++ Ejercicio1.java
@@ -0,0 +1,10 @@
+// Osmer David Cardona Mejia
+// NC: 202410050140
+// Este programa seria para indicar el nombre mio
+public class Ejercicio1
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" Hola , soy Osmer David Cardona Mejia");
+    }
+}
