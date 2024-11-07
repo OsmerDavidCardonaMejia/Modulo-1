9029b0f9c2eb79a2e2cee88e2196f1d9c6eedc8b
OsmerDavidCardonaMejia <juliomidencearias2000@gmail.com>
2024-11-06 19:27:29 -0600

    Operaciones Aritmeticas
    Crear un programa que imprima en consola todas las operaciones aritmetica de dos numeros enteros(suma, resta, multiplicacion, divicion).

---
+++ Ejercicio2.java
@@ -0,0 +1,18 @@
+// Osmer David Cardona Mejia
+// NC: 202410050140
+// Este programa seria para dar a conocer las operaciones aritmeticas
+public class Ejercicio2
+{
+    public static void main(String[] args)
+    {
+        int n1 = 20;
+        int n2 = 10;
+
+        System.out.println("Suma: " + (n1 + n2));
+        System.out.println("Resta: " + (n1 - n2));
+        System.out.println("Multiplicacion: " + (n1 * n2));
+        System.out.println("Division: " + (n1 / n2));
+        System.out.println("Modulo: " + (n1 % n2));
+    }
+}
+
