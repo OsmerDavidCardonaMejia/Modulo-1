d537330747fc38181f91e31ed5e7b50706daa975
OsmerDavidCardonaMejia <juliomidencearias2000@gmail.com>
2024-11-06 19:36:30 -0600

    Ciclo de numeros pares
    Escribe un programa que imprima mediante un ciclo los números pares del 2 al 100

---
+++ Ejercicio7.java
@@ -0,0 +1,13 @@
+// Osmer David Cardona Mejia
+// 202410050140
+// Este programa da a refelejarar que va a imprimir los numeros pares del 2 al 100
+public class Ejercicio7 
+{
+    public static void main(String[] args) 
+    {
+        for (int i = 2 ; i <= 100 ; i += 2)
+        {
+            System.out.println(i);
+        }
+    }
+}
