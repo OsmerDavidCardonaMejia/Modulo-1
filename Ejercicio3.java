25a9ace6cd620794b3b90afe4e52cf9b07bf2c88
OsmerDavidCardonaMejia <juliomidencearias2000@gmail.com>
2024-11-06 19:30:22 -0600

    Variables
    Dadas las variables de tipo int M = 6, T = 1, K = -10 indicar si la evaluación de estas expresiones daría como resultado verdadero o falso:
    M > T
    T / K == -5
    (M+T == 7) || (M-T == 5)

---
+++ Ejercicio3.java
@@ -0,0 +1,15 @@
+// Osmer David Cardona Mejia
+// NC: 202410050140
+// Este programa da a ver si es verdadero o flaso (true)o(false)
+public class Ejercicio3
+{
+    public static void main(String[] args)
+    {
+        int M = 6, T = 1, K = -10;
+
+        System.out.println("M > T: " + (M > T)); 
+        System.out.println("T / K == -5: " + (T / K == -5)); 
+        System.out.println("(M+T == 7) || (M-T == 5): " + ((M + T == 7) || (M - T == 5)));
+        
+    }
+}
