27f0491d88f02a0c9317b20b1261bc5ae0899741
OsmerDavidCardonaMejia <juliomidencearias2000@gmail.com>
2024-11-06 19:32:02 -0600

    Arreglo
    Crear un arrelo que guarde e imprima 10 nombres de tus compañeros de la clase

---
+++ Ejercicio4.java
@@ -0,0 +1,14 @@
+// Osmer David Cardona Mejia
+// NC:202410050140
+// Este seria para guardar los nombres de mi compañeros 
+public class Ejercicio4 
+{
+    public static void main(String[] args) 
+    {
+        String[] nc = {"Danlis", "Mirian", "Arnold", "Dimas", "Emanuel", "Daniela", "Blanca", "Jose", "Jeyson", "Mayely"};
+
+        for (int i = 0; i < nc.length; i++) {
+            System.out.println("Nombre " + (i + 1) + ": " + nc[i]);
+        }
+    }
+}
